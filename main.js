56ca045c7b8349d20615aa89f3c8f2615c3843c9
Roberto Sánchez <roberto.sanchez2024@hilahasl.com>
2025-03-04 09:34:11 -0600

    :sparkles: New change

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-¿'
+//Conexión con HTML (Seleccionar elementos)
+
+const songForm = document.getElementById('song_form');
+const playList = document.getElementById('play_list');
+
+//Función para agregar una canción a la lista
+function addSong(songName, artistName, songUrl){
+    const listSong =  document.createElement('li');
+    listSong.innerHTML = `
+            <strong> ${songName}</strong>
+            - ${artistName}
+            <a href="${songUrl}" target="_blank" >Reproducir</a>
+            <button class="delete-btn">Eliminar</button>
+    `;
+
+    //Agregar al HTML
+    playList.appendChild(listSong);
+
+}
+
+//Manejo de envio de formulario (eventos)
+
+songForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+
+    //Obtener datos del formulario
+    const songName = document.getElementById("song_name").value;
+    const artistName = document.getElementById("artist_name").value;
+    const songUrl = document.getElementById("url_song").value;
+
+    addSong(songName, artistName, songUrl);
+    
+    songForm.reset();
+} )
